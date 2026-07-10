20dab383b652911002d02219f022559de9dc41bf
Gabriel <gab3ng67@gmail.com>
2026-07-10 14:56:23 +0800

    logis + tik = logistik

--- src/server.js
+++ src/server.js
@@ -1,13 +1,16 @@
 import express from 'express'
 import router from './routes/router.js'
+import dotenv from 'dotenv'
 
 const app = express()
-const port = 5000
+dotenv.config
+const port = process.env.LOCAL_PORT
+const host = process.env.DB_HOST
 
 
 app.use(express.json())
 app.use(router)
 
 app.listen(port, () => {
-    console.log(`to the http://localhost:${port}`)
+    console.log(`to the http://${host}:${port}`)
 })
